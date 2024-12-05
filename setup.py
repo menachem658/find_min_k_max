1ec2276482615bdb589e6a979449b45cfa5e0f9e
menachem sokolik <69851967+menachem658@users.noreply.github.com>
2024-12-05 18:02:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,24 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name='find_min_k_max',
     version='0.1.0',
-    description='A Python package to calculate the minimum k_max for statistical bounds.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/username/find_min_k_max',
-    author='menachem sokolik',
-    author_email='menachemsokolik@gmail.com',
-    license='MIT',
-    packages=find_packages(),
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     install_requires=[
         'numpy',
         'scipy',
         'matplotlib',
-        'seaborn',
+        'seaborn'
     ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.7',
+    author='menachem sokolik',
+    author_email='menachemsokolik@gmail.com',
+    description='A package for statistical computations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
