cf687f253d2715524d461a2e6a0638a8887f8914
menachem sokolik <69851967+menachem658@users.noreply.github.com>
2024-12-05 14:46:20 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
+from .core import (
+    lower_edge,
+    upper_edge,
+    CI_for_non_frequent_symbols,
+    expected_max_E_ri,
+    expected_log_S_with_max_E_ri,
+)
 
+__version__ = "0.1.0"
+__description__ = "A Python package for statistical analysis of non-frequent symbols."
