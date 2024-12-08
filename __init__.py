17268fba2d75df4b40fee99bfb6bf0a631517d16
menachem sokolik <69851967+menachem658@users.noreply.github.com>
2024-12-08 21:55:11 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ from .core import (
     lower_edge,
     upper_edge,
     CI_for_non_frequent_symbols,
+    binofit,
+    compute_pci_mine,
     expected_max_E_ri,
     expected_log_S_with_max_E_ri,
 )
